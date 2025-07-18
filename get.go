pardnchiu/golang-redis-fallback

feat: Add Redis fallback mechanism with health checks, memory storage, and logging

get.go
@@ -1,0 +1,107 @@
+package golangRedisFallback
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+)
+
+func (rf *RedisFallback) Get(key string) (interface{}, error) {
+	rf.mutex.RLock()
+	isHealth := rf.isHealth
+	rf.mutex.RUnlock()
+
+	if isHealth {
+		return rf.getFromRedis(key)
+	}
+	return rf.getFromMemory(key)
+}
+
+func (rf *RedisFallback) getFromRedis(key string) (interface{}, error) {
+	ctx := context.Background()
+
+	// * Result does not exist or error
+	// * Check if the item exists in cache
+	if cached, ok := rf.cache.Load(key); ok {
+		item := cached.(Cache)
+
+		// * Item is expired
+		if isExpired(item) {
+			rf.cache.Delete(key)
+			rf.removeJSONFile(key)
+
+			return nil, rf.logger.error(nil, "Not found")
+		}
+
+		go rf.syncToRedis(key, item)
+
+		return item.Data, nil
+	}
+
+	for i := 0; i < rf.config.Options.MaxRetry; i++ {
+		result, err := rf.redis.Get(ctx, key).Result()
+		// * Result exists and no error
+		if err == nil {
+			var item Cache
+			// * Parse the JSON data
+			if json.Unmarshal([]byte(result), &item) == nil {
+				// * Add to memory cache
+				rf.cache.Store(key, item)
+				return item.Data, nil
+			}
+		}
+	}
+
+	rf.logger.info("[getFromRedis] Switching to fallback mode")
+	rf.mutex.Lock()
+	rf.changeToFallbackMode()
+	rf.mutex.Unlock()
+
+	return rf.getFromMemory(key)
+}
+
+func (rf *RedisFallback) getFromMemory(key string) (interface{}, error) {
+	if result, ok := rf.cache.Load(key); ok {
+		item := result.(Cache)
+
+		// * Item is expired
+		if isExpired(item) {
+			rf.cache.Delete(key)
+
+			return nil, rf.logger.error(nil, "Not found")
+		}
+
+		// * Check if the item is valid
+		return item.Data, nil
+	}
+
+	return rf.loadFromFile(key)
+}
+
+func (rf *RedisFallback) loadFromFile(key string) (interface{}, error) {
+	path := getPath(rf.config, key)
+
+	// * Check if the file exists
+	data, err := os.ReadFile(path.filepath)
+	if err != nil {
+		return nil, rf.logger.error(nil, "Not found")
+	}
+
+	var item Cache
+	// * Parse the JSON data
+	if err := json.Unmarshal(data, &item); err != nil {
+		return nil, rf.logger.error(nil, "Failed to parse")
+	}
+
+	// * Check if the item is expired
+	if isExpired(item) {
+		rf.removeJSONFile(key)
+
+		return nil, rf.logger.error(nil, "Not found")
+	}
+
+	// * Update memory cache
+	rf.cache.Store(key, item)
+
+	return item.Data, nil
+}

instance.go
@@ -1,0 +1,184 @@
+package golangRedisFallback
+
+import (
+	"context"
+	"fmt"
+	"net/smtp"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func New(c Config) (*RedisFallback, error) {
+	// * Initialize logger
+	logger, err := initLogger(c)
+	if err != nil {
+		return nil, err
+	}
+
+	// * Initialize Redis
+	redisClient := initRedis(c)
+
+	// * Validate options
+	c = validateOptions(c)
+
+	// * Create fallback db directory
+	if err := os.MkdirAll(c.Options.DBPath, 0755); err != nil {
+		logger.error(err, "Failed to create DB folder")
+	}
+
+	ctx := context.Background()
+	redisFallback := &RedisFallback{
+		config:  c,
+		logger:  logger,
+		redis:   redisClient,
+		context: ctx,
+		writer: &Writer{
+			config:  c,
+			logger:  logger,
+			queue:   make(chan WriteRequest, c.Options.MaxQueue),
+			timer:   time.NewTicker(c.Options.TimeToWrite),
+			pending: make(map[string]interface{}),
+		},
+	}
+
+	// * check Redis connection
+	if err := redisClient.Ping(ctx).Err(); err != nil {
+		// * fallback mode
+		logger.error(err, "Failed to connect, Starting fallback mode")
+		redisFallback.changeToFallbackMode()
+	} else {
+		// * normal mode
+		logger.info("Starting normal mode")
+		redisFallback.changeToNormalMode()
+	}
+
+	go redisFallback.writer.start()
+	go redisFallback.startMemoryCleanup()
+
+	return redisFallback, nil
+}
+
+func initLogger(c Config) (*Logger, error) {
+	if c.Log == nil {
+		c.Log = &Log{
+			Path:    defaultLogPath,
+			Stdout:  false,
+			MaxSize: defaultLogMaxSize,
+		}
+	}
+	if c.Log.Path == "" {
+		c.Log.Path = defaultLogPath
+	}
+	if c.Log.MaxSize <= 0 {
+		c.Log.MaxSize = defaultLogMaxSize
+	}
+
+	logger, err := newLogger(c.Log)
+	if err != nil {
+		return nil, fmt.Errorf("Can not initialize logger: %v", err)
+	}
+	return logger, nil
+}
+
+func initRedis(c Config) *redis.Client {
+	if c.Redis == nil {
+		c.Redis = &Redis{
+			Host:     "localhost",
+			Port:     6379,
+			Password: "",
+			DB:       0,
+		}
+	}
+	if c.Redis.Host == "" {
+		c.Redis.Host = "localhost"
+	}
+	if c.Redis.Port <= 0 || c.Redis.Port > 65535 {
+		c.Redis.Port = 6379
+	}
+	if c.Redis.DB < 0 || c.Redis.DB > 15 {
+		c.Redis.DB = 0
+	}
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", c.Redis.Host, c.Redis.Port),
+		Password: c.Redis.Password,
+		DB:       c.Redis.DB,
+	})
+	return redisClient
+}
+
+func validateOptions(c Config) Config {
+	if c.Options == nil {
+		c.Options = &Options{
+			DBPath:   defaultDBPath,
+			MaxRetry: defaultMaxRetry,
+		}
+	}
+	if c.Options.DBPath == "" {
+		c.Options.DBPath = defaultDBPath
+	}
+	if c.Options.MaxRetry <= 0 {
+		c.Options.MaxRetry = defaultMaxRetry
+	}
+	if c.Options.MaxQueue <= 0 {
+		c.Options.MaxQueue = defaultMaxQueue
+	}
+	if c.Options.TimeToWrite <= 0 {
+		c.Options.TimeToWrite = defaultTimeToWrite
+	}
+	if c.Options.TimeToCheck <= 0 {
+		c.Options.TimeToCheck = defaultTimeToCheck
+	}
+	return c
+}
+
+func (rf *RedisFallback) removeJSONFile(key string) {
+	path := getPath(rf.config, key)
+	os.Remove(path.filepath)
+}
+
+func (rf *RedisFallback) Close() {
+	if rf.checker != nil {
+		rf.checker.Stop()
+	}
+	rf.writer.timer.Stop()
+	rf.redis.Close()
+}
+
+func (m *RedisFallback) sendEmail(ip string, reason string) {
+	if m.config.Email == nil {
+		return
+	}
+
+	subject := fmt.Sprintf("[IP Sentry] IP %s has been banned", ip)
+	if m.config.Email.Subject != nil {
+		str := (*m.config.Email.Subject)(ip, reason)
+		if str != "" {
+			subject = str
+		}
+	}
+	body := fmt.Sprintf("[IP Sentry] IP %s has been banned for %s", ip, reason)
+	if m.config.Email.Body != nil {
+		str := (*m.config.Email.Body)(ip, reason)
+		if str != "" {
+			body = str
+		}
+	}
+	msg := fmt.Sprintf("From: %s\r\nTo: %s\r\nCc: %s\r\nSubject: %s\r\n\r\n%s",
+		m.config.Email.From,
+		strings.Join(m.config.Email.To, ","),
+		strings.Join(m.config.Email.CC, ","),
+		subject,
+		body)
+
+	auth := smtp.PlainAuth("", m.config.Email.Username, m.config.Email.Password, m.config.Email.Host)
+	addr := fmt.Sprintf("%s:%d", m.config.Email.Host, m.config.Email.Port)
+
+	err := smtp.SendMail(addr, auth, m.config.Email.From, m.config.Email.To, []byte(msg))
+	if err != nil {
+		m.logger.error(err, "Failed to send email")
+	}
+}

set.go
@@ -1,0 +1,68 @@
+package golangRedisFallback
+
+import (
+	"context"
+	"encoding/json"
+	"reflect"
+	"strings"
+	"time"
+)
+
+func (rf *RedisFallback) Set(key string, value interface{}, ttl time.Duration) error {
+	rf.mutex.RLock()
+	isHealth := rf.isHealth
+	rf.mutex.RUnlock()
+
+	item := Cache{
+		Key:       key,
+		Data:      value,
+		Type:      reflect.TypeOf(value).String(),
+		Timestamp: time.Now().Unix(),
+	}
+
+	if ttl > 0 {
+		item.TTL = int64(ttl.Seconds())
+	}
+
+	if isHealth {
+		return rf.setToRedis(key, item)
+	}
+	return rf.setToMemory(key, item)
+}
+
+func (rf *RedisFallback) setToRedis(key string, cache Cache) error {
+	ctx := context.Background()
+
+	data, err := json.Marshal(cache.Data)
+	data = []byte(strings.Trim(string(data), "\""))
+	if err != nil {
+		return rf.logger.error(err, "Failed to parse")
+	}
+
+	for i := 0; i < rf.config.Options.MaxRetry; i++ {
+		err = rf.redis.Set(ctx, key, data, time.Duration(cache.TTL)*time.Second).Err()
+		if err == nil {
+			rf.cache.Store(key, cache)
+			return nil
+		}
+	}
+
+	rf.logger.info("[setToRedis] Switching to fallback mode")
+	rf.mutex.Lock()
+	rf.changeToFallbackMode()
+	rf.mutex.Unlock()
+
+	return rf.setToMemory(key, cache)
+}
+
+func (rf *RedisFallback) setToMemory(key string, item Cache) error {
+	rf.cache.Store(key, item)
+
+	select {
+	case rf.writer.queue <- WriteRequest{Key: key, Data: item}:
+	default:
+		return rf.writer.writeToFile(key, item)
+	}
+
+	return nil
+}

type.go
@@ -1,0 +1,95 @@
+package golangRedisFallback
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+const (
+	defaultLogPath     = "./logs/golangRedisFallback"
+	defaultLogMaxSize  = 16 * 1024 * 1024
+	defaultDBPath      = "./files/golangRedisFallback/db"
+	defaultMaxRetry    = 3
+	defaultMaxQueue    = 1000            // 最大排隊長度，預設 1000
+	defaultTimeToWrite = 3 * time.Second // 預設 Fallback 模式下寫入時間間隔
+	defaultTimeToCheck = 1 * time.Minute // 預設健康檢查時間間隔
+)
+
+type Config struct {
+	Redis   *Redis       `json:"redis"`             // Redis 設定
+	Log     *Log         `json:"log,omitempty"`     // 日誌設定
+	Options *Options     `json:"options,omitempty"` // 選項設定
+	Email   *EmailConfig `json:"email,omitempty"`   // Email 通知設定
+}
+
+type Redis struct {
+	Host     string `json:"host"`               // Redis 主機位址
+	Port     int    `json:"port"`               // Redis 連接埠
+	Password string `json:"password,omitempty"` // Redis 密碼
+	DB       int    `json:"db"`                 // Redis 資料庫編號
+}
+
+type Options struct {
+	DBPath      string        // 預設資料庫路徑
+	MaxRetry    int           // 最大重試次數，預設 3
+	MaxQueue    int           // 最大排隊長度，預設 1000
+	TimeToWrite time.Duration // Fallback 模式下寫入時間間隔，預設 3 秒
+	TimeToCheck time.Duration // 健康檢查時間間隔，預設 1 分鐘
+}
+
+type RedisFallback struct {
+	config       Config
+	logger       *Logger
+	redis        *redis.Client
+	context      context.Context
+	mutex        sync.RWMutex
+	cache        sync.Map
+	isHealth     bool
+	isRecovering atomic.Bool
+	checker      *time.Ticker
+	writer       *Writer
+}
+
+type Writer struct {
+	config  Config
+	logger  *Logger
+	mutex   sync.Mutex
+	queue   chan WriteRequest
+	pending map[string]interface{}
+	timer   *time.Ticker
+}
+
+type WriteRequest struct {
+	Key  string
+	Data interface{}
+}
+
+type Cache struct {
+	Key       string      `json:"key"`
+	Data      interface{} `json:"data"`
+	Type      string      `json:"type"`
+	Timestamp int64       `json:"timestamp"`
+	TTL       int64       `json:"ttl,omitempty"`
+}
+
+type Path struct {
+	folderPath string
+	filepath   string
+	filename   string
+}
+
+type EmailConfig struct {
+	Host     string                                 `json:"host"`
+	Port     int                                    `json:"port"`
+	Username string                                 `json:"username"`
+	Password string                                 `json:"password"`
+	From     string                                 `json:"from"`
+	To       []string                               `json:"to"`
+	CC       []string                               `json:"cc"`
+	Subject  *func(ip string, reason string) string `json:"-"` // default: "[IP Sentry] IP {ip} has been banned"
+	Body     *func(ip string, reason string) string `json:"-"` // default: "[IP Sentry] IP {ip} has been banned for {reason}"
+}

writer.go
@@ -1,0 +1,66 @@
+package golangRedisFallback
+
+import (
+	"encoding/json"
+	"os"
+	"sync"
+)
+
+func (w *Writer) start() {
+	go func() {
+		for {
+			select {
+			case req := <-w.queue:
+				w.mutex.Lock()
+				w.pending[req.Key] = req.Data
+				w.mutex.Unlock()
+			case <-w.timer.C:
+				w.write()
+			}
+		}
+	}()
+}
+
+func (w *Writer) write() {
+	w.mutex.Lock()
+	// * nothing to write
+	if len(w.pending) == 0 {
+		w.mutex.Unlock()
+		return
+	}
+
+	list := make(map[string]interface{})
+	for k, v := range w.pending {
+		list[k] = v
+	}
+	w.pending = make(map[string]interface{})
+	w.mutex.Unlock()
+
+	var wg sync.WaitGroup
+	for key, data := range list {
+		wg.Add(1)
+		go func(k string, d interface{}) {
+			defer wg.Done()
+			if item, ok := d.(Cache); ok {
+				w.writeToFile(k, item)
+			}
+		}(key, data)
+	}
+	wg.Wait()
+}
+
+func (w *Writer) writeToFile(key string, cache Cache) error {
+	path := getPath(w.config, key)
+
+	// * Create fallback db directory
+	if err := os.MkdirAll(path.folderPath, 0755); err != nil {
+		return w.logger.error(err, "Failed to create folder")
+	}
+
+	data, err := json.Marshal(cache)
+	if err != nil {
+		return w.logger.error(err, "Failed to parse")
+	}
+
+	return os.WriteFile(path.filepath, data, 0644)
+}

pardnchiu/go-redis-fallback

get.go
@@ -1,0 +1,107 @@
+package redisFallback
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+)
+
+func (rf *RedisFallback) Get(key string) (interface{}, error) {
+	rf.mutex.RLock()
+	isHealth := rf.isHealth
+	rf.mutex.RUnlock()
+
+	if isHealth {
+		return rf.getFromRedis(key)
+	}
+	return rf.getFromMemory(key)
+}
+
+func (rf *RedisFallback) getFromRedis(key string) (interface{}, error) {
+	ctx := context.Background()
+
+	// * Result does not exist or error
+	// * Check if the item exists in cache
+	if cached, ok := rf.cache.Load(key); ok {
+		item := cached.(Cache)
+
+		// * Item is expired
+		if isExpired(item) {
+			rf.cache.Delete(key)
+			rf.removeJSONFile(key)
+
+			return nil, rf.logger.error(nil, "Not found")
+		}
+
+		go rf.syncToRedis(key, item)
+
+		return item.Data, nil
+	}
+
+	for i := 0; i < rf.config.Options.MaxRetry; i++ {
+		result, err := rf.redis.Get(ctx, key).Result()
+		// * Result exists and no error
+		if err == nil {
+			var item Cache
+			// * Parse the JSON data
+			if json.Unmarshal([]byte(result), &item) == nil {
+				// * Add to memory cache
+				rf.cache.Store(key, item)
+				return item.Data, nil
+			}
+		}
+	}
+
+	rf.logger.info("[getFromRedis] Switching to fallback mode")
+	rf.mutex.Lock()
+	rf.changeToFallbackMode()
+	rf.mutex.Unlock()
+
+	return rf.getFromMemory(key)
+}
+
+func (rf *RedisFallback) getFromMemory(key string) (interface{}, error) {
+	if result, ok := rf.cache.Load(key); ok {
+		item := result.(Cache)
+
+		// * Item is expired
+		if isExpired(item) {
+			rf.cache.Delete(key)
+
+			return nil, rf.logger.error(nil, "Not found")
+		}
+
+		// * Check if the item is valid
+		return item.Data, nil
+	}
+
+	return rf.loadFromFile(key)
+}
+
+func (rf *RedisFallback) loadFromFile(key string) (interface{}, error) {
+	path := getPath(rf.config, key)
+
+	// * Check if the file exists
+	data, err := os.ReadFile(path.filepath)
+	if err != nil {
+		return nil, rf.logger.error(nil, "Not found")
+	}
+
+	var item Cache
+	// * Parse the JSON data
+	if err := json.Unmarshal(data, &item); err != nil {
+		return nil, rf.logger.error(nil, "Failed to parse")
+	}
+
+	// * Check if the item is expired
+	if isExpired(item) {
+		rf.removeJSONFile(key)
+
+		return nil, rf.logger.error(nil, "Not found")
+	}
+
+	// * Update memory cache
+	rf.cache.Store(key, item)
+
+	return item.Data, nil
+}

instance.go
@@ -1,0 +1,184 @@
+package redisFallback
+
+import (
+	"context"
+	"fmt"
+	"net/smtp"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func New(c Config) (*RedisFallback, error) {
+	// * Initialize logger
+	logger, err := initLogger(c)
+	if err != nil {
+		return nil, err
+	}
+
+	// * Initialize Redis
+	redisClient := initRedis(c)
+
+	// * Validate options
+	c = validateOptions(c)
+
+	// * Create fallback db directory
+	if err := os.MkdirAll(c.Options.DBPath, 0755); err != nil {
+		logger.error(err, "Failed to create DB folder")
+	}
+
+	ctx := context.Background()
+	redisFallback := &RedisFallback{
+		config:  c,
+		logger:  logger,
+		redis:   redisClient,
+		context: ctx,
+		writer: &Writer{
+			config:  c,
+			logger:  logger,
+			queue:   make(chan WriteRequest, c.Options.MaxQueue),
+			timer:   time.NewTicker(c.Options.TimeToWrite),
+			pending: make(map[string]interface{}),
+		},
+	}
+
+	// * check Redis connection
+	if err := redisClient.Ping(ctx).Err(); err != nil {
+		// * fallback mode
+		logger.error(err, "Failed to connect, Starting fallback mode")
+		redisFallback.changeToFallbackMode()
+	} else {
+		// * normal mode
+		logger.info("Starting normal mode")
+		redisFallback.changeToNormalMode()
+	}
+
+	go redisFallback.writer.start()
+	go redisFallback.startMemoryCleanup()
+
+	return redisFallback, nil
+}
+
+func initLogger(c Config) (*Logger, error) {
+	if c.Log == nil {
+		c.Log = &Log{
+			Path:    defaultLogPath,
+			Stdout:  false,
+			MaxSize: defaultLogMaxSize,
+		}
+	}
+	if c.Log.Path == "" {
+		c.Log.Path = defaultLogPath
+	}
+	if c.Log.MaxSize <= 0 {
+		c.Log.MaxSize = defaultLogMaxSize
+	}
+
+	logger, err := newLogger(c.Log)
+	if err != nil {
+		return nil, fmt.Errorf("Can not initialize logger: %v", err)
+	}
+	return logger, nil
+}
+
+func initRedis(c Config) *redis.Client {
+	if c.Redis == nil {
+		c.Redis = &Redis{
+			Host:     "localhost",
+			Port:     6379,
+			Password: "",
+			DB:       0,
+		}
+	}
+	if c.Redis.Host == "" {
+		c.Redis.Host = "localhost"
+	}
+	if c.Redis.Port <= 0 || c.Redis.Port > 65535 {
+		c.Redis.Port = 6379
+	}
+	if c.Redis.DB < 0 || c.Redis.DB > 15 {
+		c.Redis.DB = 0
+	}
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", c.Redis.Host, c.Redis.Port),
+		Password: c.Redis.Password,
+		DB:       c.Redis.DB,
+	})
+	return redisClient
+}
+
+func validateOptions(c Config) Config {
+	if c.Options == nil {
+		c.Options = &Options{
+			DBPath:   defaultDBPath,
+			MaxRetry: defaultMaxRetry,
+		}
+	}
+	if c.Options.DBPath == "" {
+		c.Options.DBPath = defaultDBPath
+	}
+	if c.Options.MaxRetry <= 0 {
+		c.Options.MaxRetry = defaultMaxRetry
+	}
+	if c.Options.MaxQueue <= 0 {
+		c.Options.MaxQueue = defaultMaxQueue
+	}
+	if c.Options.TimeToWrite <= 0 {
+		c.Options.TimeToWrite = defaultTimeToWrite
+	}
+	if c.Options.TimeToCheck <= 0 {
+		c.Options.TimeToCheck = defaultTimeToCheck
+	}
+	return c
+}
+
+func (rf *RedisFallback) removeJSONFile(key string) {
+	path := getPath(rf.config, key)
+	os.Remove(path.filepath)
+}
+
+func (rf *RedisFallback) Close() {
+	if rf.checker != nil {
+		rf.checker.Stop()
+	}
+	rf.writer.timer.Stop()
+	rf.redis.Close()
+}
+
+func (m *RedisFallback) sendEmail(ip string, reason string) {
+	if m.config.Email == nil {
+		return
+	}
+
+	subject := fmt.Sprintf("[IP Sentry] IP %s has been banned", ip)
+	if m.config.Email.Subject != nil {
+		str := (*m.config.Email.Subject)(ip, reason)
+		if str != "" {
+			subject = str
+		}
+	}
+	body := fmt.Sprintf("[IP Sentry] IP %s has been banned for %s", ip, reason)
+	if m.config.Email.Body != nil {
+		str := (*m.config.Email.Body)(ip, reason)
+		if str != "" {
+			body = str
+		}
+	}
+	msg := fmt.Sprintf("From: %s\r\nTo: %s\r\nCc: %s\r\nSubject: %s\r\n\r\n%s",
+		m.config.Email.From,
+		strings.Join(m.config.Email.To, ","),
+		strings.Join(m.config.Email.CC, ","),
+		subject,
+		body)
+
+	auth := smtp.PlainAuth("", m.config.Email.Username, m.config.Email.Password, m.config.Email.Host)
+	addr := fmt.Sprintf("%s:%d", m.config.Email.Host, m.config.Email.Port)
+
+	err := smtp.SendMail(addr, auth, m.config.Email.From, m.config.Email.To, []byte(msg))
+	if err != nil {
+		m.logger.error(err, "Failed to send email")
+	}
+}

set.go
@@ -1,0 +1,68 @@
+package redisFallback
+
+import (
+	"context"
+	"encoding/json"
+	"reflect"
+	"strings"
+	"time"
+)
+
+func (rf *RedisFallback) Set(key string, value interface{}, ttl time.Duration) error {
+	rf.mutex.RLock()
+	isHealth := rf.isHealth
+	rf.mutex.RUnlock()
+
+	item := Cache{
+		Key:       key,
+		Data:      value,
+		Type:      reflect.TypeOf(value).String(),
+		Timestamp: time.Now().Unix(),
+	}
+
+	if ttl > 0 {
+		item.TTL = int64(ttl.Seconds())
+	}
+
+	if isHealth {
+		return rf.setToRedis(key, item)
+	}
+	return rf.setToMemory(key, item)
+}
+
+func (rf *RedisFallback) setToRedis(key string, cache Cache) error {
+	ctx := context.Background()
+
+	data, err := json.Marshal(cache.Data)
+	data = []byte(strings.Trim(string(data), "\""))
+	if err != nil {
+		return rf.logger.error(err, "Failed to parse")
+	}
+
+	for i := 0; i < rf.config.Options.MaxRetry; i++ {
+		err = rf.redis.Set(ctx, key, data, time.Duration(cache.TTL)*time.Second).Err()
+		if err == nil {
+			rf.cache.Store(key, cache)
+			return nil
+		}
+	}
+
+	rf.logger.info("[setToRedis] Switching to fallback mode")
+	rf.mutex.Lock()
+	rf.changeToFallbackMode()
+	rf.mutex.Unlock()
+
+	return rf.setToMemory(key, cache)
+}
+
+func (rf *RedisFallback) setToMemory(key string, item Cache) error {
+	rf.cache.Store(key, item)
+
+	select {
+	case rf.writer.queue <- WriteRequest{Key: key, Data: item}:
+	default:
+		return rf.writer.writeToFile(key, item)
+	}
+
+	return nil
+}

type.go
@@ -1,0 +1,95 @@
+package redisFallback
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+const (
+	defaultLogPath     = "./logs/redisFallback"
+	defaultLogMaxSize  = 16 * 1024 * 1024
+	defaultDBPath      = "./files/redisFallback/db"
+	defaultMaxRetry    = 3
+	defaultMaxQueue    = 1000            // 最大排隊長度，預設 1000
+	defaultTimeToWrite = 3 * time.Second // 預設 Fallback 模式下寫入時間間隔
+	defaultTimeToCheck = 1 * time.Minute // 預設健康檢查時間間隔
+)
+
+type Config struct {
+	Redis   *Redis       `json:"redis"`             // Redis 設定
+	Log     *Log         `json:"log,omitempty"`     // 日誌設定
+	Options *Options     `json:"options,omitempty"` // 選項設定
+	Email   *EmailConfig `json:"email,omitempty"`   // Email 通知設定
+}
+
+type Redis struct {
+	Host     string `json:"host"`               // Redis 主機位址
+	Port     int    `json:"port"`               // Redis 連接埠
+	Password string `json:"password,omitempty"` // Redis 密碼
+	DB       int    `json:"db"`                 // Redis 資料庫編號
+}
+
+type Options struct {
+	DBPath      string        // 預設資料庫路徑
+	MaxRetry    int           // 最大重試次數，預設 3
+	MaxQueue    int           // 最大排隊長度，預設 1000
+	TimeToWrite time.Duration // Fallback 模式下寫入時間間隔，預設 3 秒
+	TimeToCheck time.Duration // 健康檢查時間間隔，預設 1 分鐘
+}
+
+type RedisFallback struct {
+	config       Config
+	logger       *Logger
+	redis        *redis.Client
+	context      context.Context
+	mutex        sync.RWMutex
+	cache        sync.Map
+	isHealth     bool
+	isRecovering atomic.Bool
+	checker      *time.Ticker
+	writer       *Writer
+}
+
+type Writer struct {
+	config  Config
+	logger  *Logger
+	mutex   sync.Mutex
+	queue   chan WriteRequest
+	pending map[string]interface{}
+	timer   *time.Ticker
+}
+
+type WriteRequest struct {
+	Key  string
+	Data interface{}
+}
+
+type Cache struct {
+	Key       string      `json:"key"`
+	Data      interface{} `json:"data"`
+	Type      string      `json:"type"`
+	Timestamp int64       `json:"timestamp"`
+	TTL       int64       `json:"ttl,omitempty"`
+}
+
+type Path struct {
+	folderPath string
+	filepath   string
+	filename   string
+}
+
+type EmailConfig struct {
+	Host     string                                 `json:"host"`
+	Port     int                                    `json:"port"`
+	Username string                                 `json:"username"`
+	Password string                                 `json:"password"`
+	From     string                                 `json:"from"`
+	To       []string                               `json:"to"`
+	CC       []string                               `json:"cc"`
+	Subject  *func(ip string, reason string) string `json:"-"` // default: "[IP Sentry] IP {ip} has been banned"
+	Body     *func(ip string, reason string) string `json:"-"` // default: "[IP Sentry] IP {ip} has been banned for {reason}"
+}

writer.go
@@ -1,0 +1,66 @@
+package redisFallback
+
+import (
+	"encoding/json"
+	"os"
+	"sync"
+)
+
+func (w *Writer) start() {
+	go func() {
+		for {
+			select {
+			case req := <-w.queue:
+				w.mutex.Lock()
+				w.pending[req.Key] = req.Data
+				w.mutex.Unlock()
+			case <-w.timer.C:
+				w.write()
+			}
+		}
+	}()
+}
+
+func (w *Writer) write() {
+	w.mutex.Lock()
+	// * nothing to write
+	if len(w.pending) == 0 {
+		w.mutex.Unlock()
+		return
+	}
+
+	list := make(map[string]interface{})
+	for k, v := range w.pending {
+		list[k] = v
+	}
+	w.pending = make(map[string]interface{})
+	w.mutex.Unlock()
+
+	var wg sync.WaitGroup
+	for key, data := range list {
+		wg.Add(1)
+		go func(k string, d interface{}) {
+			defer wg.Done()
+			if item, ok := d.(Cache); ok {
+				w.writeToFile(k, item)
+			}
+		}(key, data)
+	}
+	wg.Wait()
+}
+
+func (w *Writer) writeToFile(key string, cache Cache) error {
+	path := getPath(w.config, key)
+
+	// * Create fallback db directory
+	if err := os.MkdirAll(path.folderPath, 0755); err != nil {
+		return w.logger.error(err, "Failed to create folder")
+	}
+
+	data, err := json.Marshal(cache)
+	if err != nil {
+		return w.logger.error(err, "Failed to parse")
+	}
+
+	return os.WriteFile(path.filepath, data, 0644)
+}